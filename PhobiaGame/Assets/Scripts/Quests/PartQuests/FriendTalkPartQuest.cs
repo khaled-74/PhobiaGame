be5e0fad3ae38012d0d48597c815fe85995a7620
khaled-74 <princek_59@yahoo.com>
2022-12-03 15:26:02 +0200

    Polishing Vol 1 Finished

--- PhobiaGame/Assets/Scripts/Quests/PartQuests/FriendTalkPartQuest.cs
+++ PhobiaGame/Assets/Scripts/Quests/PartQuests/FriendTalkPartQuest.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using Quests;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Quests.PartQuests
 {
     public class FriendTalkPartQuest : Quest
     {
-        public Order SelectedOrder;
+        [SerializeField] private TMP_Text talkText;
+        [SerializeField] private float durationToDisappear = 5;
+        [HideInInspector] public Order SelectedOrder;
         
         public override IEnumerator Progress()
         {
             SelectedOrder = (Order)Random.Range(0, 3);
+            talkText.text = "I need, " + SelectedOrder.ToString();
             Debug.Log("Friend Is Talking");
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(durationToDisappear);
+            talkText.gameObject.SetActive(false);
             Debug.Log("Friend Is Stopped Talking");
             IsFinished = true;
         }
